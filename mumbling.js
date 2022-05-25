4e2d999181a1493f5dfcfb78029e2f128605c0e4
jiara <ji.arakani@gmail.com>
2022-05-25 16:37:42 -0400

    best version!

---
+++ mumbling.js
@@ -0,0 +1,19 @@
+/*
+This function transforms strings into the following format:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+
+function accum(s) {
+    return s.toLowerCase()
+            .split("")
+            .map((c, i) => c.toUpperCase() + c.repeat(i))
+            .join("-")
+}
+
+console.log(accum("ZpglnRxqenU"))
+console.log(accum("NyffsGeyylB"))
+console.log(accum("MjtkuBovqrU"))
+console.log(accum("EvidjUnokmM"))
+console.log(accum("HbideVbxncC"))
